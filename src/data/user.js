8294002c895becdc4fffd8eafc2f66cb23a932af
Vinishireis <nishimuravinicius28@gmail.com>
2024-09-22 19:12:39 -0300

    React Portfolio

---
+++ src/data/user.js
@@ -0,0 +1,83 @@
+const INFO = {
+	main: {
+		title: "Portfolio React by Vinishireis",
+		name: "Vinícius Nishimura",
+		email: "nishimuravinicius28@gmail.com",
+		logo: "../logo.png",
+	},
+
+	socials: {
+		github: "https://github.com/Vinishireis",
+		linkedin: "https://www.linkedin.com/in/vin%C3%ADcius-nishimura-reis",
+		instagram: "https://www.instagram.com/Vinishireis",
+		stackoverflow: "https://stackoverflow.com/users/23758668/vinicius-nishimura",
+		twitter: "https://x.com/NishimuraReis?t=EJQD0z4kGsp7_GLeoBkGKw&s=09"
+	},
+
+	homepage: {
+		title: "Full-stack web and mobile app developer.",
+		description:
+			"I am a backend developer with expertise in Node.js. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a developer.",
+	},
+
+	about: {
+		title: "Olá sou Vinícius Nishimura Reis, sou desenvolvedor WEB / Mobile",
+		description:
+			"I've developed a solid foundation in web development through my specialization in internet systems. Over the years, I've gained experience working with various programming languages and technologies like MySQL, Java, JavaScript, HTML, CSS, and more. Many of the projects I've contributed to are available for review and collaboration. I believe in the power of shared knowledge and am always open to feedback and suggestions for improvement. If you're interested in any of my work, feel free to explore the code and offer insights. I'm always eager to collaborate and grow through learning from others.",
+	},
+
+	articles: {
+		title: "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
+		description:
+			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
+	},
+
+	projects: [
+		{
+			title: "Projeto Vita Activa",
+			description:
+				"Este aplicativo foi desenvolvido com o objetivo de fornecer serviços fictícios nas áreas de Web Design, Desenvolvimento Mobile e Design Gráfico. Ele inclui várias funcionalidades, como a exibição de serviços em uma lista, a criação de uma barra de menu e a integração de cores específicas para o tema.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "Visualizar esse Projeto",
+			link: "https://github.com/Vinishireis/OnDev_App_React_Project",
+		},
+
+		{
+			title: "Project 2",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Project 3",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Project 4",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Project 5",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+	],
+};
+
+export default INFO;
